5964cf5b7554b6f8884d033e626e4a1ff8d30763
Renan Morais <renanc.morais@gmail.com>
2022-08-16 08:10:57 -0300

    feat: continuação do header home

---
+++ src/components/headers/counter.ts
@@ -0,0 +1,21 @@
+const counter = (date: Date) => {
+    const countDownDate = date.getTime();
+    const now = new Date().getTime();
+
+    const distance = countDownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    return {
+        distance,
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+}
+
+export { counter };
