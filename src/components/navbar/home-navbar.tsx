e68b0dc65668d5e785f8d0c384d3cd0b6daec06f
Vitor Resende <vitoresende@gmail.com>
2022-08-19 16:56:00 -0300

    Creating shadow and updating css when scrolling

--- src/components/navbar/home-navbar.tsx
+++ src/components/navbar/home-navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Collapse,
   Navbar,
@@ -11,27 +11,41 @@ import {
 import LogoGDG from "../../assets/images/LogoGDG";
 import styles from "../../styles/Navbar.module.css";
 
-const NavbarHome = (args) => {
+const NavbarHome = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isScrolling, setScrolling] = useState(false);
+
+  // TODO: update with correct menu
+  const menu = [{name: "Sobre", ref: "#"}, {name: "Palestrantes", ref: "#"}, {name: "Patrocinadores", ref: "#"}]
 
   const toggle = () => setIsOpen(!isOpen);
 
+  useEffect(() => {
+    const onScroll = () => {
+        if(window.pageYOffset === 0){
+            setScrolling(false);
+        } else {
+            setScrolling(true);
+        }
+    };
+    // clean up code
+    window.removeEventListener('scroll', onScroll);
+    window.addEventListener('scroll', onScroll, { passive: true });
+    return () => window.removeEventListener('scroll', onScroll);
+    }, []);
+
   return (
-    <div className={styles.navbar_fixed}>
-      <Navbar {...args} color="faded" light expand="md">
-        <NavbarBrand href="/"><LogoGDG /></NavbarBrand>
+    <div className={isScrolling? styles.navbar_fixed_scrolling : styles.navbar_fixed}>
+      <Navbar className={styles.main_navbar} color="faded" light expand="md">
+        {isScrolling ? <NavbarBrand className={styles.nav_brand}><LogoGDG color="white"/></NavbarBrand> : null}
         <NavbarToggler onClick={toggle} className="mr-2" />
         <Collapse className={styles.collapse_menu} isOpen={isOpen} navbar>
           <Nav className="ms-auto" navbar>
-            <NavItem >
-              <NavLink className={styles.nav_link} href="/about/">Sobre</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className={styles.nav_link} href="/speakers/">Palestrantes</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className={styles.nav_link} href="/sponsors/">Patrocinadores</NavLink>
-            </NavItem>
+            {menu.map((menuItems, index) =>
+                <NavItem key={"nav-item-" + index}>
+                    <NavLink className={styles.nav_link} style={{ color: isScrolling ? 'white': 'rgba(0,0,0,.55)' }} href={menuItems.ref}>{menuItems.name}</NavLink>
+                </NavItem>
+            )}
           </Nav>
         </Collapse>
       </Navbar>
