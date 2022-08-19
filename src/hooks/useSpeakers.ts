7f026c87bee49b3823d96f5b4227099239a0e044
TecJP <tec.joao.freitas@gmail.com>
2022-08-19 15:30:58 -0300

    Working on a carousel in speakers section

---
+++ src/hooks/useSpeakers.ts
@@ -0,0 +1,124 @@
+const speakers = [
+  {
+    id: 1,
+    speaker_name: 'Jéssica Costa',
+    location: {
+      city: 'Uberlândia',
+      uf: 'MG'
+    },
+    topic: 'Machine Learning',
+    title: 'GDE'
+  },
+  {
+    id: 2,
+    speaker_name: 'Keilla Menezes',
+    location: {
+      city: 'Uberlândia',
+      uf: 'MG'
+    },
+    topic: 'WEB',
+    title: 'GDE'
+  },
+  {
+    id: 3,
+    speaker_name: 'Hulgo Leornardo',
+    location: {
+      city: 'Itumbiara',
+      uf: 'GO'
+    },
+    topic: 'Data Science',
+    title: 'GDE'
+  },
+  {
+    id: 4,
+    speaker_name: 'João Pedro',
+    location: {
+      city: 'Itumbiara',
+      uf: 'GO'
+    },
+    topic: 'WEB',
+    title: 'GDE'
+  },
+  {
+    id: 5,
+    speaker_name: 'Renan Morais',
+    location: {
+      city: 'Goiânia',
+      uf: 'GO'
+    },
+    topic: 'Angular',
+    title: 'Gupy'
+  },
+  {
+    id: 6,
+    speaker_name: 'Erika Conca',
+    location: {
+      city: 'Goiânia',
+      uf: 'GO'
+    },
+    topic: 'Java',
+    title: 'Zup'
+  },
+  {
+    id: 7,
+    speaker_name: 'Tacio Medeiros',
+    location: {
+      city: 'Uberlândia',
+      uf: 'MG'
+    },
+    topic: 'Flutter',
+    title: 'Vita'
+  },
+  {
+    id: 8,
+    speaker_name: 'Elisabeth Mamede',
+    location: {
+      city: 'Uberlândia',
+      uf: 'MG'
+    },
+    topic: 'QA',
+    title: 'PicPay'
+  },
+  {
+    id: 9,
+    speaker_name: 'Sandro Moreira',
+    location: {
+      city: 'Rio Verde',
+      uf: 'Go'
+    },
+    topic: 'Tensor Flow',
+    title: 'UFG'
+  },
+  {
+    id: 10,
+    speaker_name: 'Alvaro Viebrantz',
+    location: {
+      city: 'Cuiabá',
+      uf: 'MT'
+    },
+    topic: 'IoT',
+    title: 'Google'
+  },
+  {
+    id: 11,
+    speaker_name: 'Fernando Sedrez',
+    location: {
+      city: 'Pelotas',
+      uf: 'Rio Grande do Sul'
+    },
+    topic: 'Data Science',
+    title: 'Globo'
+  },
+  {
+    id: 12,
+    speaker_name: 'Fernando Foster',
+    location: {
+      city: 'São José do Rio Preto',
+      uf: 'SP'
+    },
+    topic: 'IA',
+    title: 'Noonly'
+  },
+]
+
+export default speakers;
