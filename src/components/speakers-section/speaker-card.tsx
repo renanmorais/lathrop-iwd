8bbf03ecbc16d14b7db2b8a634af1e5b5430f72e
Renan Morais <renanc.morais@gmail.com>
2022-08-19 22:15:23 -0300

    Carousel is working finally

--- src/components/speakers-section/speaker-card.tsx
+++ src/components/speakers-section/speaker-card.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import React, { useState } from "react";
 
+import ProfileImage from '../../assets/images/ProfileImage.svg';
 import styles from '../../styles/Speakers.module.css'
 
 interface SpeakerCardProps {
@@ -25,7 +26,7 @@ const SpeakerCard: React.FC<SpeakerCardProps> = ({
   return (
     <>
       <div className={styles.card_content}>
-        <Image className={styles.card_image} src="/" alt={`Foto ${speaker_name}`} width="90%" height="90%" />
+        <Image className={styles.card_image} src={ProfileImage} alt={`Foto ${speaker_name}`} width="90%" height="90%" />
         <h2 className={styles.card_name}>{speaker_name}</h2>
         <p className={styles.card_location}>{`${location.city} ${location.uf}`}</p>
         <p className={styles.card_topic}>Tema</p>
