9b33901a38e73001081cc0aed9b59f42a787db55
Tacio Medeiros <tacio.medeiros@vitta.me>
2022-08-24 17:53:03 -0300

    fix: fixing sponsors links

--- src/hooks/useSponsors.ts
+++ src/hooks/useSponsors.ts
@@ -8,12 +8,12 @@ const sponsors = {
   support: {
     items: [
       { name: 'GDG Goiânia', logo: '/gdg-goiania.png', url: 'https://gdg.community.dev/gdg-goiania/' },
-      { name: 'GDG Itumbiara', logo: '/gdg-itumbiara.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
-      { name: 'GDG Jatai', logo: '/gdg-jatai.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
-      { name: 'GDG Mogi Guaçu', logo: '/gdg-mogi-guacu.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
-      { name: 'GDG São José Rio Preto', logo: '/gdg-sao-jose-rio-preto.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
-      { name: 'GDG São João da Boa Vista', logo: '/gdg-sao-joao-boa-vista.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
-      { name: 'GDG Rio Verde', logo: '/gdg-rio-verde.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
+      { name: 'GDG Itumbiara', logo: '/gdg-itumbiara.png', url: 'https://gdg.community.dev/gdg-itumbiara/' },
+      { name: 'GDG Jatai', logo: '/gdg-jatai.png', url: 'https://gdg.community.dev/gdg-jatai/' },
+      { name: 'GDG Mogi Guaçu', logo: '/gdg-mogi-guacu.png', url: 'https://gdg.community.dev/gdg-mogi-guacu/' },
+      { name: 'GDG São João da Boa Vista', logo: '/gdg-sao-joao-boa-vista.png', url: 'https://gdg.community.dev/gdg-sao-joao-da-boa-vista/' },
+      { name: 'GDG São José Rio Preto', logo: '/gdg-sao-jose-rio-preto.png', url: 'https://gdg.community.dev/gdg-sao-jose-do-rio-preto/' },
+      { name: 'GDG Rio Verde', logo: '/gdg-rio-verde.png', url: 'https://gdg.community.dev/gdg-rio-verde/' },
       { name: 'GDG Uberlândia', logo: '/gdg-uberlandia.png', url: 'https://gdg.community.dev/gdg-uberlandia/' },
     ]
   }
