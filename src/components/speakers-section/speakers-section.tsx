9fb22d93b6869c3bc7116d11c3703bd78b6c8f69
TecJP <tec.joao.freitas@gmail.com>
2022-09-26 09:01:34 -0300

    Initialize with the speakers details modal.

--- src/components/speakers-section/speakers-section.tsx
+++ src/components/speakers-section/speakers-section.tsx
@@ -9,14 +9,13 @@ import {
   CarouselIndicators,
   Button,
 } from "reactstrap";
-import speakers from '../../hooks/useSpeakers';
-import useWindowDimensions from '../../hooks/useWindowDimensions';
+import speakers from "../../hooks/useSpeakers";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 
-
-import styles from '../../styles/Speakers.module.css'
+import styles from "../../styles/Speakers.module.css";
 import SpeakerCard from "./speaker-card";
 
-const SpeakersSection: React.FC = ({ }) => {
+const SpeakersSection: React.FC = ({}) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [isSSR, setIsSSR] = useState(true);
   const { width } = useWindowDimensions();
@@ -28,22 +27,23 @@ const SpeakersSection: React.FC = ({ }) => {
 
   const speakersChunk = (array: Array<Speaker>, size: number) => {
     return Array.from({ length: Math.ceil(array.length / size) }, (v, i) =>
-      array.slice(i * size, i * size + size,),
-    )
+      array.slice(i * size, i * size + size)
+    );
   };
-
 
   let _chunckSize = 4;
   if (!isSSR && width != null && width < 1076) _chunckSize = 1;
   const _speakersChuncked = speakersChunk(speakers, _chunckSize);
 
   const next = () => {
-    const nextIndex = activeIndex === _speakersChuncked.length - 1 ? 0 : activeIndex + 1;
+    const nextIndex =
+      activeIndex === _speakersChuncked.length - 1 ? 0 : activeIndex + 1;
     setActiveIndex(nextIndex);
   };
 
   const previous = () => {
-    const nextIndex = activeIndex === 0 ? _speakersChuncked.length - 1 : activeIndex - 1;
+    const nextIndex =
+      activeIndex === 0 ? _speakersChuncked.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
 
@@ -51,28 +51,27 @@ const SpeakersSection: React.FC = ({ }) => {
     setActiveIndex(newIndex);
   };
 
-
-  const displaySpeakers = _speakersChuncked.map((speakersList: Array<Speaker>, index: number) => {
-
-    return (
-      <CarouselItem
-        key={`${index}-carousel-item`}
-      >
-        <div className={styles.carousel_inner}>
-          {speakersList.map((speaker: Speaker) => {
-            const _itemId: number = speaker.id;
-            return (
-              <Col key={`${_itemId}-carousel-col`} className={styles.card_container}>
-                <SpeakerCard
-                  {...speaker}
-                />
-              </Col>
-            )
-          })}
-        </div>
-      </CarouselItem>
-    )
-  })
+  const displaySpeakers = _speakersChuncked.map(
+    (speakersList: Array<Speaker>, index: number) => {
+      return (
+        <CarouselItem key={`${index}-carousel-item`}>
+          <div className={styles.carousel_inner}>
+            {speakersList.map((speaker: Speaker) => {
+              const _itemId: number = speaker.id;
+              return (
+                <Col
+                  key={`${_itemId}-carousel-col`}
+                  className={styles.card_container}
+                >
+                  <SpeakerCard {...speaker} />
+                </Col>
+              );
+            })}
+          </div>
+        </CarouselItem>
+      );
+    }
+  );
 
   return (
     <>
@@ -80,32 +79,34 @@ const SpeakersSection: React.FC = ({ }) => {
         <div id="speakers" className={styles.container}>
           <h1>Palestrantes</h1>
           <p>
-            O time de palestrantes do Devfest Cerrado 2022 traz grandes nomes da área técnica e referências em liderança de comunidades. Reunimos em um só evento Google Developer Experts (GDEs), Tech Leads, pessoas desenvolvedoras e principalmente pessoas resolvedoras de problemas. Esse é um evento que visa fomentar a comunidade de tecnologia, indo além de conhecimento técnico, contando com muita interatividade, conexões e diversidade.
+            O time de palestrantes do Devfest Cerrado 2022 traz grandes nomes da
+            área técnica e referências em liderança de comunidades. Reunimos em
+            um só evento Google Developer Experts (GDEs), Tech Leads, pessoas
+            desenvolvedoras e principalmente pessoas resolvedoras de problemas.
+            Esse é um evento que visa fomentar a comunidade de tecnologia, indo
+            além de conhecimento técnico, contando com muita interatividade,
+            conexões e diversidade.
           </p>
           <div className={styles.cards}>
             <Carousel
               activeIndex={activeIndex}
               next={next}
               previous={previous}
-              ride='carousel'
+              ride="carousel"
               className={styles.carousel}
             >
               {displaySpeakers}
             </Carousel>
           </div>
-          {/*<div className={styles.button_container}>
-            <a
-              color="info"
-              href="/speakers"
-            >
+          <div className={styles.button_container}>
+            <a color="info" href="/speakers">
               Ver todos
             </a>
-  </div>*/}
+          </div>
         </div>
       </Container>
     </>
   );
-}
+};
 
 export default SpeakersSection;
-
