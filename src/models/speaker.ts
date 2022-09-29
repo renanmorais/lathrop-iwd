f12e2f23ca5af8da28073aa4e06bf997bd25cfe5
Renan Morais <renanc.morais@gmail.com>
2022-09-29 10:26:09 -0300

    Finished the modal footer

--- src/models/speaker.ts
+++ src/models/speaker.ts
@@ -1,10 +1,17 @@
 export interface Speaker {
-    id: number;
-    name: string;
-    tech: string;
-    topic: string;
-    title: string;
-    mini_bio: string;
-    photo: string;
-    companyTitle: string;
+  id: number;
+  name: string;
+  tech: string;
+  topic: string;
+  title: string;
+  mini_bio: string;
+  photo: string;
+  companyTitle: string;
+  social_media: {
+    instagram: string;
+    twitter: string;
+    linkedIn: string;
+    github: string;
+    website: string;
+  };
 }
