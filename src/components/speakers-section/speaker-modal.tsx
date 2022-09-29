d57dff75c529f4b87d671bdef3297caa71458317
TecJP <tec.joao.freitas@gmail.com>
2022-09-29 10:26:09 -0300

    Finished the modal footer

--- src/components/speakers-section/speaker-modal.tsx
+++ src/components/speakers-section/speaker-modal.tsx
@@ -2,12 +2,19 @@
 
 import speakers from "hooks/useSpeakers";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
-import { faTwitter, faInstagram, faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faTwitter,
+  faInstagram,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faArrowPointer } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import styles from '../../styles/Speakers.module.css'
+import styles from "../../styles/Speakers.module.css";
+import { Speaker } from "models/speaker";
 
 interface ModalProps {
   modalOpen: boolean;
@@ -16,37 +23,78 @@ interface ModalProps {
 }
 
 const SpeakerModal: React.FC<ModalProps> = ({ id, modalOpen, modalToggle }) => {
-  const speaker = speakers.find(speaker => {
-    if (speaker.id === id) {
-      return speaker;
+  const [speaker, setSpeaker] = useState<Speaker>({} as Speaker);
+
+  useEffect(() => {
+    for (let speaker of speakers) {
+      if (speaker.id === id) {
+        setSpeaker(speaker);
+      }
     }
-  });
+  }, []);
 
   return (
-    <Modal isOpen={modalOpen} toggle={modalToggle} centered size="lg" scrollable>
+    <Modal
+      isOpen={modalOpen}
+      toggle={modalToggle}
+      centered
+      size="lg"
+      scrollable
+    >
       <ModalHeader toggle={modalToggle} className={styles.modal_header}>
         <div className={styles.modal_speaker_info_container}>
-          <Image unoptimized className={styles.card_image} src={speaker!.photo} alt={`Foto ${speaker!.name}`} width="90%" height="90%" />
+          <Image
+            unoptimized
+            className={styles.card_image}
+            src={speaker.photo}
+            alt={`Foto ${speaker.name}`}
+            width="100%"
+            height="100%"
+          />
           <div className={styles.modal_speaker_info_content}>
-            <h1>{speaker!.name}</h1>
-            <p>{speaker!.tech}</p>
-            <p>{speaker!.title}</p>
+            <h1>{speaker.name}</h1>
+            <p>{speaker.tech}</p>
+            <p>{speaker.title}</p>
           </div>
         </div>
       </ModalHeader>
       <ModalBody className={styles.modal_body}>
-        <h2>{speaker!.topic}</h2>
-        <p>{speaker!.mini_bio}</p>
+        <h2>{speaker.topic}</h2>
+        <p>{speaker.mini_bio}</p>
       </ModalBody>
       <ModalFooter className={styles.modal_footer}>
-        <a href="#"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
-        <a href="#"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
-        <a href="#"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
-        <a href="#"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
+        {speaker.social_media && (
+          <>
+            {speaker.social_media.twitter && (
+              <a target="_blank" href={speaker.social_media.twitter}>
+                <FontAwesomeIcon icon={faTwitter} size="2x" />
+              </a>
+            )}
+            {speaker.social_media.instagram && (
+              <a target="_blank" href={speaker.social_media.instagram}>
+                <FontAwesomeIcon icon={faInstagram} size="2x" />
+              </a>
+            )}
+            {speaker.social_media.github && (
+              <a target="_blank" href={speaker.social_media.github}>
+                <FontAwesomeIcon icon={faGithub} size="2x" />
+              </a>
+            )}
+            {speaker.social_media.linkedIn && (
+              <a target="_blank" href={speaker.social_media.linkedIn}>
+                <FontAwesomeIcon icon={faLinkedin} size="2x" />
+              </a>
+            )}
+            {speaker.social_media.website && (
+              <a target="_blank" href={speaker.social_media.website}>
+                <FontAwesomeIcon icon={faArrowPointer} size="2x" />
+              </a>
+            )}
+          </>
+        )}
       </ModalFooter>
     </Modal>
   );
-}
+};
 
 export default SpeakerModal;
-
