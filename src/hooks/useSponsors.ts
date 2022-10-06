a852a08df72ad2ea1c1652b1a2965ca7e17a1c92
Tacio Medeiros <tacio.medeiros@vitta.me>
2022-10-06 12:20:14 -0300

    fix: ajustando cota aliare

--- src/hooks/useSponsors.ts
+++ src/hooks/useSponsors.ts
@@ -19,14 +19,14 @@ const sponsors = {
     name: 'Minas Frescal',
     items: [
       { name: 'Sankhya', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fsankhya.png?alt=media', url: 'https://www.sankhya.com.br/' },
+      { name: 'Aliare', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Faliare.png?alt=media', url: 'https://www.aliare.co/' }
     ]
   },
   bronze: {
     name: 'Meia Cura',
     items: [
       { name: 'Zup', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fzup.png?alt=media', url: 'https://www.zup.com.br' },
-      { name: 'CI&T', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fciet.png?alt=media', url: 'https://ciandt.com/br' },
-      { name: 'Aliare', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Faliare.png?alt=media', url: 'https://www.aliare.co/' }
+      { name: 'CI&T', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fciet.png?alt=media', url: 'https://ciandt.com/br' }
     ]
   },
   ruby: {
