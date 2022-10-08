29c945032e925f39af3187346b8d50adb0a9e997
Renan Morais <renanc.morais@gmail.com>
2022-10-08 18:48:41 -0300

    feat: adicionando sessao de tickets

--- src/components/tickets-section/ticket-card.tsx
+++ src/components/tickets-section/ticket-card.tsx
@@ -5,23 +5,38 @@ import React from "react";
 import styles from '../../styles/Tickets.module.css'
 
 
-interface TicketProps extends TicketBatch {
+interface TicketProps {
+    ticket: TicketBatch
 }
 
-const TicketCard: React.FC<TicketProps> = (ticket) => {
+const TicketCard: React.FC<TicketProps> = ({ ticket }) => {
 
     const buttonClasses = ['DangerButton'];
 
-    if (ticket.outOfSale) buttonClasses.push('DisableButton');
+    if (ticket.isOutOfSale()) buttonClasses.push('DisableButton');
+
+    const mapPurchaseButton = () => {
+        if (ticket.firstSaleDate > new Date()) {
+            return (
+                <>
+                    <span>* Não iniciado </span>
+                    <br />
+                    <span className={styles.TicketNotInitiadedDate}>{ticket.firstSaleDate.toLocaleDateString('pt-br')}</span>
+                </>);
+        }
+
+        else
+            return (<div >
+                <a target="_blank" className={buttonClasses.join(' ')} rel="noreferrer" href={ticket.link}>Comprar</a>
+            </div>)
+    }
 
     return (
         <>
-            <h4 className={styles.TicketItemTitle} >1º Lote</h4>
-            <p className={styles.TicketItemDescription}>Com camiseta</p>
-            <p className={styles.TicketItemPrice}>R$ 70</p>
-            <div >
-                <a target="_blank" className={buttonClasses.join(' ')} href="#">Comprar</a>
-            </div>
+            <h4 className={styles.TicketItemTitle} >{ticket.title}</h4>
+            <p className={styles.TicketItemDescription}>{ticket.description}</p>
+            <p className={styles.TicketItemPrice}>R$ {ticket.value}</p>
+            {mapPurchaseButton()}
         </>
     );
 }
