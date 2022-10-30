56b93915841eec883c55bc5d13ad58178fde0213
Renan Morais <renanc.morais@gmail.com>
2022-10-30 20:39:42 -0300

    fix: alterando variavel de ambiente server

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -9,6 +9,6 @@ const configValues = {
 
 const dev = process.env.NODE_ENV == 'development';
 
-export const server = dev ? 'http://localhost:3000' : '';
+export const server = dev ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_SITE_URL;
 
 export default configValues;
