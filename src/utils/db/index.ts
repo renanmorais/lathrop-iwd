70d3eead618dbb6e3fd9fdd5e76bbc8db71baf09
Renan Morais <renanc.morais@gmail.com>
2022-10-30 20:09:08 -0300

    fix: corrigindo tipos

--- src/utils/db/index.ts
+++ src/utils/db/index.ts
@@ -1,23 +1,24 @@
 import admin from 'firebase-admin';
 
-let db;
+
+interface Database extends admin.firestore.Firestore {
+}
+
+let db: Database;
 if (!admin.apps.length) {
-    try {
-        if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
-            throw new Error('FIREBASE_SERVICE_ACCOUNT is not defined');
-        }
-
-        const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-        admin.initializeApp({
-            credential: admin.credential.cert(serviceAccount)
-        });
-
-        db = admin.firestore();
-
-        db.settings({ ignoreUndefinedProperties: true });
-    } catch (error) {
-        console.log('Firebase admin initialization error', error);
+    if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+        throw new Error('FIREBASE_SERVICE_ACCOUNT is not defined');
     }
+
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount)
+    });
+
+    db = admin.firestore();
+
+    db.settings({ ignoreUndefinedProperties: true });
+
 }
 else {
     db = admin.firestore();
