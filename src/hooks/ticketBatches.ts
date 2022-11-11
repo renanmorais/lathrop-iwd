e4b90ef717c404ea2e1048fd697ffc155a7034f4
Renan Morais <renanc.morais@gmail.com>
2022-11-11 17:39:01 -0300

    fix: updating kit+camiseta final sale day.

--- src/hooks/ticketBatches.ts
+++ src/hooks/ticketBatches.ts
@@ -1,7 +1,7 @@
 import { TicketBatch } from "models/ticket-batch";
 
 const ticketBatches: Array<TicketBatch> = [
-    new TicketBatch({ description: 'Kit + camiseta', firstSaleDate: new Date('2022-11-01'), finalSaleDate: new Date('2022-11-15'), title: '3º Lote', link: 'https://doity.com.br/devfest-cerrado-2022#registration', value: 150 }),
+    new TicketBatch({ description: 'Kit + camiseta', firstSaleDate: new Date('2022-11-01'), finalSaleDate: new Date('2022-11-11'), title: '3º Lote', link: 'https://doity.com.br/devfest-cerrado-2022#registration', value: 150 }),
     new TicketBatch({ description: 'Kit', firstSaleDate: new Date('2022-11-01'), finalSaleDate: new Date('2022-11-25'), title: '3º Lote', link: 'https://doity.com.br/devfest-cerrado-2022#registration', value: 130 }),
     new TicketBatch({ description: 'Kit + camiseta', firstSaleDate: new Date('2022-09-26'), finalSaleDate: new Date('2022-10-31T23:59:00'), title: '2º Lote', link: 'https://doity.com.br/devfest-cerrado-2022#registration', value: 120 }),
     new TicketBatch({ description: 'Kit', firstSaleDate: new Date('2022-09-26'), finalSaleDate: new Date('2022-10-31T23:59:00'), title: '2º Lote', link: 'https://doity.com.br/devfest-cerrado-2022#registration', value: 100 }),
