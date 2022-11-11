25c6a3ebf13c86745ec8b7dd11e1c4e3095ce0bf
Renan Morais <renanc.morais@gmail.com>
2022-11-10 23:47:49 -0300

    feat: add nextjs for schedule

---
+++ src/front-features/schedule.ts
@@ -0,0 +1,11 @@
+import { server } from 'helpers/config';
+import { Schedule } from 'models/schedule';
+
+export const getSchedule = async (): Promise<Schedule[]> => {
+    const url = `${server}/api/v1/schedule`;
+
+    console.log(url);
+    const res = await fetch(url);
+    const schedule = await res.json();
+    return schedule;
+}
