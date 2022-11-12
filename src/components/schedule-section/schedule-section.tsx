24dffff2faeee0ed92c50a8e2361f5aab4aaf301
Renan Morais <renanc.morais@gmail.com>
2022-11-11 22:46:50 -0300

    validacao se nulo

--- src/components/schedule-section/schedule-section.tsx
+++ src/components/schedule-section/schedule-section.tsx
@@ -13,49 +13,49 @@ import styles from "../../styles/Schedule.module.css";
 import { Schedule } from "models/schedule";
 
 interface SpeakersSectionProps {
-    speakers: Array<Speaker>,
-    schedule: Array<Schedule>
+  speakers: Array<Speaker>,
+  schedule: Array<Schedule>
 }
-  
+
 const ScheduleSection: React.FC<SpeakersSectionProps> = ({ speakers, schedule }) => {
-    
+
   return (
     <>
       {speakers.length &&
-      <Container>
-        <div id="schedule">
-          <h4>Agenda</h4>
-        </div>
-        {
-            schedule.map((schedule, index)=> {
-                return (
+        <Container>
+          <div id="schedule">
+            <h4>Agenda</h4>
+          </div>
+          {
+            schedule.map((schedule, index) => {
+              return (
                 <Row key={`schedule-${index}`} className={styles.row_content}>
-                    <ScheduleTime initialTime={schedule.start} endTime={schedule.end} />
-                    <Col xxl={11} sm={12}>
-                        <Row className={styles.height100p}>
-                        {
-                            schedule.speeches.map((speech, index) => {
-                                const speaker = speakers.find(speakerObj => speakerObj.id === speech.speaker_id);
-                                console.log("speaker: " + JSON.stringify(speaker))
-                                if(speech?.topic){
-                                    return (
-                                        <ScheduleCard key={`speech-${schedule.start}-${schedule.end}-${index}`} lgValue={12/schedule.speeches.length} {...speaker} topic={speech.topic} />
-                                    )
-                                } else {
-                                    return (
-                                        <ScheduleCard key={`speech-${schedule.start}-${schedule.end}-${index}`} lgValue={12/schedule.speeches.length} {...speaker} />
-                                    )
-                                }
-                                
-                            }) 
-                        }
-                        </Row>
-                    </Col>
+                  <ScheduleTime initialTime={schedule.start} endTime={schedule.end} />
+                  <Col xxl={11} sm={12}>
+                    <Row className={styles.height100p}>
+                      {
+                        schedule.speeches?.map((speech, index) => {
+                          const speaker = speakers.find(speakerObj => speakerObj.id === speech.speaker_id);
+                          console.log("speaker: " + JSON.stringify(speaker))
+                          if (speech?.topic) {
+                            return (
+                              <ScheduleCard key={`speech-${schedule.start}-${schedule.end}-${index}`} lgValue={12 / schedule.speeches.length} {...speaker} topic={speech.topic} />
+                            )
+                          } else {
+                            return (
+                              <ScheduleCard key={`speech-${schedule.start}-${schedule.end}-${index}`} lgValue={12 / schedule.speeches.length} {...speaker} />
+                            )
+                          }
+
+                        })
+                      }
+                    </Row>
+                  </Col>
                 </Row>)
             })
-        }
-      </Container>
-    }   
+          }
+        </Container>
+      }
     </>
   );
 };
