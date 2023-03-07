ca355f2914a7e6d70803abfa8d60c0e09d9b2b9f
Renan Morais <renanc.morais@gmail.com>
2023-03-07 10:30:39 -0300

    fix price lote

--- src/hooks/ticketBatches.ts
+++ src/hooks/ticketBatches.ts
@@ -2,7 +2,7 @@ import { TicketBatch } from "models/ticket-batch";
 
 const ticketBatches: Array<TicketBatch> = [
     new TicketBatch({ description: 'Entrada + Kit', firstSaleDate: new Date('2023-01-01'), finalSaleDate: new Date('2023-03-06'), title: '1º Lote', link: 'https://doity.com.br/iwd-cerrado-2023#registration', value: 55 }),
-    new TicketBatch({ description: 'Entrada + Kit', firstSaleDate: new Date('2023-03-07'), finalSaleDate: new Date('2023-03-20'), title: '2º Lote', link: 'https://doity.com.br/iwd-cerrado-2023#registration', value: 65 }),
+    new TicketBatch({ description: 'Entrada + Kit', firstSaleDate: new Date('2023-03-07'), finalSaleDate: new Date('2023-03-20'), title: '2º Lote', link: 'https://doity.com.br/iwd-cerrado-2023#registration', value: 60 }),
     new TicketBatch({ description: 'Entrada + Kit', firstSaleDate: new Date('2023-03-20'), finalSaleDate: new Date('2023-04-15'), title: '3º Lote', link: 'https://doity.com.br/iwd-cerrado-2023#registration', value: 75 }),
 ]
 
