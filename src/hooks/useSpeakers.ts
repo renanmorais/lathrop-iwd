4d4907beb06a9997c64095204c8ea7528bb6c673
Erika Ferra Conca <erikafconca@gmail.com>
2023-03-25 00:15:53 -0300

    insercao palestrantes

--- src/hooks/useSpeakers.ts
+++ src/hooks/useSpeakers.ts
@@ -5,7 +5,7 @@ const speakers = [
         "/speakers/silvia_coelho.jpeg",
     name: "Silvia Coelho",
     tech: "Carreira",
-    topic: "",
+    topic: "De CAPS LOCK a TOP VOICE",
     mini_bio:
         "Silvia Coelho é uma engenheira paraense de apenas 49 anos, mãe de dois e idealizadora da comunidade Elas Programam. Após quase 10 anos de pausa na carreira, decidiu retornar ao mercado de tecnologia. Foi aí que se deparou com o gap de gênero no setor e criou a Elas Programam em 2017 para mudar esse cenário. 05 anos depois, Elas Programam conta com uma comunidade engajada de mais de 100 mil pessoas em diversos canais digitais. Em março de 2022, Silvia foi reconhecida pelo LinkedIn como LinkedIn Top Voices e LinkedIn Creator. Hoje ela é CTO da startup jurídica Forum Hub.",
     title: "LinkedIn Top Voices e LinkedIn Creator",
@@ -29,7 +29,7 @@ const speakers = [
         "Mestra em engenharia de computação e desenvolvedora backend. Através da criação de conteúdos, sonho impactar vidas de forma positiva com rompimento de barreiras e empoderamento tecnológico, reconstruindo a área tecnológica como um espaço acessível e pertencente a todas as pessoas. ",
     companyTitle: "Spacecoding",
     social_media: {
-      instagram: "https://instagram.com/elasprogramam",
+      instagram: "https://instagram.com/spacecoding",
       twitter: "https://twitter.com/",
       linkedIn: "https://www.linkedin.com/in/spacecoding/",
       github: "https://github.com/",
@@ -42,7 +42,7 @@ const speakers = [
         "/speakers/mikaeri_ohana.jpg",
     name: "Mikaeri Ohana Estevam Candido",
     tech: "Machine learning/TensorFlow",
-    topic: "Creating scalable ML solutions to support big techs evolution",
+    topic: "De cientista de dados a gerente de produto de dados: os motivos pelos quais me mudei - e o que aprendi com isso",
     mini_bio:
         "Mikaeri Ohana é formada em análise e desenvolvimento de sistemas pela FATEC e faz mestrado na Unicamp. Ela é a primeira mulher brasileira indicada como Microsoft Most Valuable Professional em Inteligência Artificial e Google Developer Expert em Machine Learning. Trabalha com tecnologia desde 2015, e focou sua carreira em ciência de dados e machine learning em 2019. Atualmente ela trabalha com ciência de dados na CI&T e é criadora de conteúdo na @explicami, ajudando a aumentar o número de pessoas especializadas em dados no Brasil e no mundo ",
     title: "Google Developer Expert ",
@@ -64,8 +64,8 @@ const speakers = [
     topic: "",
     mini_bio:
         "Karina Tronkos (mais conhecida como Nina) é mentora de design na Apple Developer Academy e já trabalhou em empresas como Globo, Accenture e Hurb. Foi 5x vencedora do Scholarship da Apple (2017-2021), é TEDx speaker e criadora de conteúdo no Nina Talks que conta com +120mil entusiastas e apaixonados por design e tecnologia.",
-    title: "Mentora de design na Apple Developer Academy ",
-    companyTitle: "",
+    title: "Mentora de design ",
+    companyTitle: "Apple Developer Academy",
     social_media: {
       instagram: "https://www.instagram.com/ninatalks/ ",
       twitter: "https://twitter.com/",
@@ -75,12 +75,12 @@ const speakers = [
     },
   },
   {
-    id: 4,
+    id: 5,
     photo:
         "/speakers/bianca.jpeg",
     name: "Bianca Ximenes",
-    tech: "Machine learning/TensorFlow",
-    topic: "Creating scalable ML solutions to support big techs evolution",
+    tech: "Machine learning/TensorFlow e Assistentes pessoais/chatbots",
+    topic: "ChatGPT: parece até que tá vivo!",
     mini_bio:
         "Bianca foi nomeada pelo Google Developers em 2021 como uma das 21 mulheres ao redor do mundo que estão abrindo novos caminhos de relevância na tecnologia. Ela foi Head de Inteligência Artificial na Gupy e responsável pela Gaia, IA que impactou 30 milhões de pessoas em seleções. Também é Google Developer Expert em Machine Learning e Doutora em Ciência da Computação pela UFPE. Seu trabalho e pesquisa abordam o desenvolvimento de sistemas responsáveis de ML. Bianca já mentorou mais de 150 startups, palestrou para mais de 10.000 pessoas, e publicou pesquisas em revistas e conferências internacionais",
     title: "Google Developer Expert ",
@@ -94,25 +94,25 @@ const speakers = [
     },
   },
   {
-  id: 5,
+  id: 6,
   photo:
       "/speakers/ana_neri.jpg",
-  name: "Ana Neri",
-  tech: "Tecnologias web",
-  topic: "",
-  mini_bio: "Ana Neri é uma engenheira de Software Sênior que atua com projetos usando Javascript/Typescript e NestJS, ama desenvolver projetos divertidos e compartilhar conhecimento com a comunidade dos aprendizados que tira de tudo isso de forma gratuita, em todas as redes sociais como ananeridev. No tempo livre uma aspirante a baixista, mãe de pet e puxadora de ferro!",
-    title: "",
-  companyTitle: "",
-  social_media: {
-    instagram: "https://www.instagram.com/ananeridev/",
-    twitter: "https://twitter.com/",
-    linkedIn: "https://www.linkedin.com/in/ananeridev/",
-    github: "https://github.com/",
-    website: "https://www.google.com/",
+      name: "Ana Neri ",
+      tech: "Tecnologias web",
+      topic: "Você não conhece o desenvolvimento orientado a testes",
+      mini_bio: "Ana Neri é uma engenheira de Software Sênior que atua com projetos usando Javascript/Typescript e NestJS, ama desenvolver projetos divertidos e compartilhar conhecimento com a comunidade dos aprendizados que tira de tudo isso de forma gratuita, em todas as redes sociais como ananeridev. No tempo livre uma aspirante a baixista, mãe de pet e puxadora de ferro!",
+    title: "LinkedIn Creator e WomenTechMakers Ambassador",
+    companyTitle: "Senior Software Engineer Pravaler",
+      social_media: {
+        instagram: "https://www.instagram.com/ananeridev/ ",
+        twitter: "https://twitter.com/",
+        linkedIn: "https://www.linkedin.com/in/ananeridev/",
+        github: "https://http://github.com/ananeridev",
+        website: "https://www.google.com/",
     },
   },
   {
-    id: 6,
+    id: 7,
     photo:
     "/speakers/lelemaine.jpg",
     name: "Letícia Moreira ",
@@ -131,7 +131,7 @@ const speakers = [
     },
   },
   {
-    id: 7,
+    id: 8,
     photo:
         "/speakers/vanessa.png",
     name: "Vanessa Guitta ",
@@ -139,8 +139,8 @@ const speakers = [
     topic: "Carreira e Posicionamento: competências além do currículo",
     mini_bio:
       "CEO da Elas Projetam e Professora de graduação em Empreendedorismo e Inovação e de pós-graduação em Gestão de Projetos. Atua no ecossistema de inovação como empreendedora, consultora e mentora de startups e de profissionais de projetos. Com os métodos e os resultados da Elas Projetam, passou a ser reconhecida internacionalmente como liderança no desenvolvimento de perfis e qualificação de mulheres em gestão de projetos, recebendo selo da iImpact como Top #3 Startup de Impacto no ODS 8 (trabalho decente e crescimento econômico) na América Latina.",
-    title: "Mentora de design na Apple Developer Academy ",
-	companyTitle: "",
+    title: "CEO ",
+	companyTitle: "Elas Projetam",
     social_media: {
       instagram: "https://www.instagram.com/vanguitta/ ",
       twitter: "https://twitter.com/",
@@ -150,7 +150,7 @@ const speakers = [
     },
   },
   {
-    id: 8,
+    id: 9,
     photo:
     "/speakers/taina.jpeg",
     name: "Taina Rodrigues",
@@ -164,6 +164,175 @@ const speakers = [
       instagram: "https://www.instagram.com/taiina.rodrigues/ ",
       twitter: "https://twitter.com/",
       linkedIn: "https://www.linkedin.com/in/tainavrodrigues/",
+      github: "https://github.com/",
+      website: "",
+    },
+  },
+  {
+    id: 10,
+    photo:
+      "/speakers/thielle.jpeg",
+    name: "Thielle Cathia de Paula dos Santos",
+    tech: "Gestão e metodologias ágeis, Carreira e desenvolvimento pessoal",
+    topic: "Gestão do Tempo: O dia tem 24 horas para todos você que não sabe administrar. Será?",
+    mini_bio:
+      " Gerente de Projetos no Governo do Estado de Goiás. Diretora do projeto social Programa Donizetti. Catequista na igreja católica.",
+    title: "Gerente de Projetos no Governo do Estado de Goiás",
+	companyTitle: "Governo do Estado de Goiás",
+    social_media: {
+      instagram: "https://www.instagram.com/thiellecathia/ ",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://br.linkedin.com/in/thiellecathia",
+      github: "https://github.com/",
+      website: "",
+    },
+  },
+  {
+    id: 11,
+    photo:
+      "/speakers/jess.jpeg",
+    name: "Jéssica da Silva Costa",
+    tech: "Análise de dados, Cloud",
+    topic: "Otimizando Custos no BigQuery",
+    mini_bio:
+      "Jéssica é analista de dados na TAG IMF e GDE em Machine Learning. Atualmente faz mestrado em Ciência da Computação com foco em Ciência de Dados. Formada em Sistemas de Informação pela UFS, também já atuou com organização de comunidades de Tecnologia.",
+    title: "GDE Machine learning",
+    companyTitle: "Analista de dados",
+    social_media: {
+      instagram: "https://instagram.com/jessicacostainfo",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/jessica-costa-info/",
+      github: "https://github.com/",
+      website: "https://medium.com/jessica-costa",
+    },
+  },
+  {
+    id: 12,
+    photo:
+      "/speakers/vanessacamargo.jpeg",
+    name: "Vanessa Camargo",
+    tech: "Carreira e desenvolvimento pessoal",
+    topic: "O que não te contam sobre transição de carreira",
+    mini_bio:
+	"Graduada em Ciência da Computação e Mestra na área de Computação Gráfica, passou 10 anos na carreira acadêmica até se encantar com o poder das comunidades de startups e do empreendedorismo. Atuou, então, como gestora de incubadora, gerente de programas e com desenvolvimento de negócios B2B. Hoje atua como Community Manager no time de Google Developers LATAM, unindo, assim, suas principais paixões: tecnologia, educação e impacto.",
+    title: "Community Manager",
+    companyTitle: "TVC para o Google",
+    social_media: {
+      instagram: "https://instagram.com/vanessaxcamargo",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/vanessaxcamargo/",
+      github: "https://github.com/",
+      website: "",
+    },
+  },
+  {
+    id: 13,
+    photo:
+      "/speakers/beatrizk.jpeg",
+    name: "Beatriz Kaylene Sardeiro Pereira",
+    tech: "Gestão e metodologias ágeis, Carreira e desenvolvimento pessoal, Diversidade e inclusão, OKRs",
+    topic: "Apertem o cinto, o piloto é LEAN!",
+    mini_bio:
+	"Condecorada pela Câmara Municipal de Goiânia por seu destaque como Mulher na Tecnologia, atuou como Scrum Master do primeiro Hub Agro do Centro-oeste liderando pessoas e gerindo processos para o lançamento de diversas Startups de sucesso, é Especialista em Gestão de Projetos e Engenheira de Produção. Hoje atua como Agile Team Leader em equipes multidisciplinares no Desenvolvimento de Soluções e Gestão de iniciativas de melhorias internas.",
+    title: "Agile Team Leader",
+    companyTitle: "DB1 Group ",
+    social_media: {
+      instagram: "https://www.instagram.com/beatrizkaylene/",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/beatrizkaylene/",
+      github: "https://github.com/",
+      website: "",
+    },
+  },
+  {
+    id: 14,
+    photo:
+      "/speakers/lorenagon.jpeg",
+    name: "Lorena Gonçalves",
+    tech: "Gestão e metodologias ágeis, Carreira e desenvolvimento pessoal",
+    topic: "Como as Mulheres de Negócios Estão Impulsionando o Futuro da Tecnologia!",
+    mini_bio: "Business Analyst, Líder de comunidade Area64, Mais de 6 anos com Inovação e tecnologia para o agro, grande bagagem com negócios, startups e comunidades.",
+    title: "Business Analyst",
+    companyTitle: "Agronegócio Tecnologia Inovação " ,
+    social_media: {
+      instagram: "https://www.instagram.com/lorena.gon/",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/lorenagon/",
+      github: "https://github.com/",
+      website: "https://linktech.bio/lorenagon",
+    },
+  },
+  {
+    id: 16,
+    photo:
+      "/speakers/palloma.jpeg",
+    name: "Palloma Cristina Pereira Lobo",
+    tech: "Mobile",
+    topic: "Design System na visão da engenharia, porquê usar, apoiar e começar",
+    mini_bio:
+      "29 anos,  mãe de duas crianças engenheira de software android e muito curiosa",
+    title: "Android Developer",
+	companyTitle: "PagSeguro PagBank",
+    social_media: {
+      instagram: "https://www.instagram.com/",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/palloma-lobo-67324095/",
+      github: "https://github.com/",
+      website: "https://www.odisseialab.com/",
+    },
+  },
+  {
+    id: 17,
+    photo:
+      "/speakers/amanda.jpeg",
+      name: "Amanda Pereira Santos",
+      tech: "UX/UI",
+      topic: "UX Writing: escrita para produtos digitais",
+      mini_bio:
+        "Amanda é publicitária, escritora, professora, mentora, palestrante, especialista em Influência Digital: Conteúdo e Estratégia, especialista em Marketing, Branding e Experiência Digital, com MBA em Comunicação e Eventos, MBA em Marketing Estratégico e Mestrado de título próprio em Comunicação Empresarial e Corporativa. Como docente, já ensinou Branded Content: Marketing de Conteúdo, Redação Publicitária, Marketing Digital para escritores e Estratégia de Design de Negócios.",
+      title: "Publicitária, Escritora",
+    companyTitle: "UX Writer, Copywriter, Tech Writer",
+      social_media: {
+        instagram: "https://www.instagram.com/",
+        twitter: "https://twitter.com/",
+        linkedIn: "https://www.linkedin.com/in/amandapsnt",
+        github: "https://github.com/",
+        website: "http://www.amandapsantos.com",
+    },
+  },
+  {
+    id: 18,
+    photo:
+      "/speakers/francielly.jpeg",
+    name: "Francielly de Moraes Santos",
+    tech: "Mobile, Carreira e desenvolvimento pessoal",
+    topic: "Como escolher uma arquitetura para aplicações Android?",
+    mini_bio:
+      "Sou uma goianinha do pé rachado, daquelas ama um pequi, graduada em Sistemas de Informação pela UFG e especialista em Engenharia de Software pela Unicamp, trabalho com desenvolvimento há 10, e hoje sou Dev Android Senior no LuizaLabs, que é o braço tecnológico da Magazine Luiza. Compartilhar conhecimento é comigo mesma, já ajudei e ainda ajudo muitas pessoas com mentoria de carreira. No meu tempo livre gosto muito de beber um vinho jogando um boardgame ou lendo um livro (não pode faltar a parte nerd né ;) ).",
+    title: "Senior Android Developer ",
+	companyTitle: "Luizalabs",
+    social_media: {
+      instagram: "https://www.instagram.com/",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/franms/",
+      github: "https://github.com/",
+      website: "",
+    },
+  },
+  {
+    id: 19,
+    photo: "/speakers/jaqueline.jpeg",
+    name: "Jaqueline Alves Ribeiro",
+    tech: "IoT, Acessibilidade",
+    topic: " Tecnologia Assistiva: IOT e acessibilidade",
+    mini_bio:
+      "Professora e pesquisadora no IF Goiano Campus Ceres. Se dedica na pesquisa, ensino e extensão com temas relacionados a Internet das Coisas (IOT), Inovação e Tecnologia assistiva. Graduada em Ciência da Computação (PUC-GO), Mestre em Ciências (UFU) e Doutora em Engenharia de Sistemas Eletrônicos e Automação (UnB). Co-Founder da Comunidade MulheresGo, Líder da Comunidade StartupGO, Mentora voluntária da Techstars startup weekend, Campus Party, Latinoware e Nasa. Mentora de Inovação de Goiás.",
+    title: "Professora e pesquisadora",
+	companyTitle: "IF Goiano",
+    social_media: {
+      instagram: "https://instagram.com/profa_jaquelineribeiro?igshid=YmMyMTA2M2Y=",
+      twitter: "https://twitter.com/",
+      linkedIn: "https://www.linkedin.com/in/jaqueline-ribeiro-a78a6082/",
       github: "https://github.com/",
       website: "",
     },
