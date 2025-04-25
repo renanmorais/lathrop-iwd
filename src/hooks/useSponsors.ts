845b4c54e0bd810f195989095c0d963e9b1c9071
Diego Sampaio <renanc.morais@gmail.com>
2025-04-25 11:20:49 -0300

    feat/sponsors

--- src/hooks/useSponsors.ts
+++ src/hooks/useSponsors.ts
@@ -15,161 +15,108 @@ const sponsors = {
     ],
   },
   diamond: {
-    name: "Cora Coralina",
+    name: "Patrocinadores",
     items: [
-      // {
-      //   name: "Porto Digital",
-      //   logo: "/patrocinio/portoDigital.svg",
-      //   url: "https://www.portodigital.org/",
-      // },
-      // {
-      //   name: "Hub Goiás",
-      //   logo: "/parceiros/hub.png",
-      //   url: "https://hubgoias.org/",
-      // },
-      // {
-      //   name: "SECTI",
-      //   logo: "/patrocinio/secti.png",
-      //   url: "https://goias.gov.br/inovacao/",
-      // },
-      // {
-      //   name: "Senac Infinite",
-      //   logo: "/patrocinio/senacInfinite.png",
-      //   url: "https://www.go.senac.br/senacInfinite",
-      // },
-      // { name: 'TQI', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Ftqi.png?alt=media', url: 'https://www.tqi.com.br/' },
+      {
+        name: "Google Developer Groups",
+        logo: "/supports/googleDevelopers.png",
+        url: "https://developers.google.com",
+      },
+      {
+        name: "Globo",
+        logo: "/patrocinio/globo.png",
+        url: "https://globo.gupy.io/#xd_co_f=YWNiOTIyMTctYjU3Zi00NDk2LTgwMGEtYWE3N2E3NDhlMmUz~",
+      },
     ],
   },
   golden: {
-    name: "Patrocinadores",
+    name: "",
     items: [
       {
         name: "Cilia Digital",
         logo: "/patrocinio/cilia.png",
         url: "https://www.cilia.com.br/",
       },
-      // {
-      //   name: "Máxima Tech",
-      //   logo: "/patrocinio/maximaTech.png",
-      //   url: "https://maximatech.com.br/",
-      // },
-      // {
-      //   name: "ACSoftware",
-      //   logo: "/patrocinio/acSoftware.png",
-      //   url: "https://acsoftware.com.br/",
-      // },
-      // {
-      //   name: "PodCafé da TI",
-      //   logo: "/parceiros/podCafe.png",
-      //   url: "https://podcafe.com.br/",
-      // },
-      // {
-      //   name: "Sebrae",
-      //   logo: "/patrocinio/sebrae.jpeg",
-      //   url: "https://vitrine.sebraego.com.br/",
-      // },
-      // {
-      //   name: "Intertech",
-      //   logo: "/patrocinio/intertech.png",
-      //   url: "https://internationalidiomas.com.br/",
-      // },
-      // {
-      //   name: "International",
-      //   logo: "/patrocinio/international.png",
-      //   url: "https://internationalidiomas.com.br/",
-      // },
       {
-        name: "Globo",
-        logo: "/patrocinio/globo.png",
-        url: "https://globo.gupy.io/#xd_co_f=YWNiOTIyMTctYjU3Zi00NDk2LTgwMGEtYWE3N2E3NDhlMmUz~",
+        name: "Hub Goiás",
+        logo: "/patrocinio/hubGoias.png",
+        url: "https://hubgoias.org/",
       },
-      // {
-      //   name: "Happy",
-      //   logo: "/patrocinio/happy.jpg",
-      //   url: "https://www.vemserhappy.com.br/",
-      // },
+      {
+        name: "Aliare",
+        logo: "/patrocinio/aliare.png",
+        url: "https://www.aliare.co/",
+      },
+      {
+        name: "FIAP + Alura",
+        logo: "/patrocinio/aluraFIAP.png",
+        url: "https://www.fiap.com.br/fiap-alura/",
+      },
+      {
+        name: "Intertech",
+        logo: "/patrocinio/intertechNew.png",
+        url: "https://internationalidiomas.com.br/",
+      },
     ],
   },
   // silver: {
   //   name: "Silver",
   //   items: [
-  //     { name: 'Sankhya', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fsankhya.png?alt=media', url: 'https://www.sankhya.com.br/' },
-  //     { name: 'Aliare', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Faliare.png?alt=media', url: 'https://www.aliare.co/' },
-  //     { name: 'Asa Consultoria', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fasa.png?alt=media', url: 'https://www.asaconsultoria.com.br/' }
+  //     {
+  //       name: "",
+  //       logo: "",
+  //       url: "",
+  //     },
   //   ],
   // },
   // bronze: {
   //   name: "Bronze",
   //   items: [
   //     {
-  //       name: "Imã",
-  //       logo: "/parceiros/ima.jpeg",
-  //       url: "https://www.iwdcerrado.com.br",
+  //       name: "",
+  //       logo: "",
+  //       url: "",
   //     },
-  //     { name: 'CI&T', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fciet.png?alt=media', url: 'https://ciandt.com/br' },
   //   ],
   // },
   support: {
-    name: "Apoio estratégico",
+    name: "Apoio",
     items: [
       {
-        name: "Hub Goiás",
-        logo: "/parceiros/hub.png",
-        url: "https://hubgoias.org/",
+        name: "Black Stars",
+        logo: "/parceiros/blackStars.png",
+        url: "https://blackstarsbr.com/",
       },
-      // {
-      //   name: "Hub Cerrado",
-      //   logo: "/parceiros/hubCerrado.png",
-      //   url: "https://hubcerrado.com.br/",
-      // },
-      // {
-      //   name: "Black Stars",
-      //   logo: "/parceiros/blackStars.png",
-      //   url: "https://blackstarsbr.com/",
-      // },
-      // { name: "PorteraTech", logo: "/parceiros/porteraTech.png", url: "" },
-      // {
-      //   name: "StartupGO",
-      //   logo: "/parceiros/startupGO.png",
-      //   url: "https://startupgo.org/",
-      // },
-      // {
-      //   name: "MulheresGO",
-      //   logo: "/parceiros/mulheresGo.png",
-      //   url: "https://sites.google.com/view/comunidademulheresgo/p%C3%A1gina-inicial?authuser=3",
-      // },
-      // { name: "Acieg", logo: "/patrocinio/acieg.jpg" },
-      // {
-      //   name: "Aliança pela inovação em Goiás",
-      //   logo: "/patrocinio/sindInformatica.jpeg",
-      // },
-      // {
-      //   name: "Prospere",
-      //   logo: "/patrocinio/prospere.jpeg",
-      //   url: "http://www.iwdcerrado.com.br/",
-      // },
-      // { name: 'Techers', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Ftechers.png?alt=media', url: 'https://www.techers.com.br/' }
     ],
   },
-  // partners: {
-  //   name: "Parceiros",
-  //   items: [
-  //     { name: "Mulheres GO", logo: "/parceiros/mulheresGo.png", url: "https://sites.google.com/view/comunidademulheresgo/p%C3%A1gina-inicial?authuser=3", },
-  //     { name: "Join Community", logo: "/parceiros/joinCommunity.png", url: "https://www.joincommunity.com.br/", },
-  //     { name: "Portera Tech", logo: "/parceiros/porteraTech.png", url: "https://www.instagram.com/porteratech", },
-  //     { name: "Hub Cerrado", logo: "/parceiros/hubCerrado.png", url: "https://hubcerrado.com.br/", },
-  //     { name: "Startup GO", logo: "/parceiros/startupGO.png", url: "https://startupgo.org/", },
-  //   ],
-  // },
+  partners: {
+    name: "Organizações parceiras",
+    items: [
+      {
+        name: "Movimento Mulheres na TI",
+        logo: "/parceiros/mulheresTI.png",
+        url: "",
+      },
+      {
+        name: "Banco do Brasil",
+        logo: "/parceiros/bancoBrasil.png",
+        url: "",
+      },
+      {
+        name: "Programaria",
+        logo: "/parceiros/programaria.png",
+        url: "",
+      },
+    ],
+  },
   // companies: {
-  //   name: "Empresas que contribuem com a comunidade local e se preocupam com seus colaboradores",
+  //   name: "",
   //   items: [
-  //     { name: "Vólus", logo: "/parceiros/volus.jpeg" },
-  //     { name: "Prodata", logo: "/parceiros/prodata.png" },
-  //     { name: "Easy Crédito", logo: "/parceiros/easyCredito.jpeg" },
-  //     { name: "Trinus", logo: "/parceiros/trinus.jpeg" },
-  //     { name: 'Uberhub', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Fuberhub.png?alt=media', url: 'http://www.uberhub.com.br/' },
-  //     // { name: 'Techers', logo: 'https://firebasestorage.googleapis.com/v0/b/devfestcerrado2022.appspot.com/o/sponsors%2Ftechers.png?alt=media', url: 'https://www.techers.com.br/' }
+  //     {
+  //       name: "",
+  //       logo: "",
+  //       url: "",
+  //     },
   //   ],
   // },
 };
